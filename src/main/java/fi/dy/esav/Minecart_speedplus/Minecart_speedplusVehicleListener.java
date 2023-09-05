05665ddd4abaa1e1038f5dd442739199e2a5cea4
YuanYuanOwO <yuanyuanowo@gmail.com>
2023-09-06 02:12:52 +0800

    GPT

--- src/main/java/fi/dy/esav/Minecart_speedplus/Minecart_speedplusVehicleListener.java
+++ src/main/java/fi/dy/esav/Minecart_speedplus/Minecart_speedplusVehicleListener.java
@@ -4,6 +4,7 @@ package fi.dy.esav.Minecart_speedplus;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Minecart;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -70,44 +71,29 @@ public class Minecart_speedplusVehicleListener implements org.bukkit.event.Liste
             this.block = cart.getWorld().getBlockAt(this.blockx, this.blocky, this.blockz);
             
             this.blockid = cart.getWorld().getBlockAt(this.blockx, this.blocky, this.blockz).getBlockData().getMaterial().toString();
-            
 
-            if ((this.blockid == Material.OAK_WALL_SIGN.toString()) || (this.blockid == Material.OAK_SIGN.toString()))
-            {
-              Sign sign = (Sign)this.block.getState();
-              String[] text = sign.getLines();
-              if (text[0].equalsIgnoreCase("[msptitle]")) {
-                  if(cart.getPassengers().size()==1) {
-                	  if(cart.getPassengers().get(0) instanceof Player) {
-                		  Player p = (Player)cart.getPassengers().get(0);
-            			  plugin.titleApi.sendTitleWithPlaceholders(p,text[1]);
-            			  plugin.titleApi.sendSubtitleWithPlaceholders(p,text[2]);
-            			  plugin.titleApi.sendActionbarWithPlaceholders(p,text[3]);
-                	  }
+
+              if ((this.blockid == Material.OAK_WALL_SIGN.toString()) || (this.blockid == Material.OAK_SIGN.toString())) {
+                  Sign sign = (Sign) this.block.getState();
+                  String[] text = sign.getLines();
+                  if (text[0].equalsIgnoreCase("[msptitle]")) {
+                      if (cart.getPassengers().size() == 1) {
+                          if (cart.getPassengers().get(0) instanceof Player) {
+                              Player p = (Player) cart.getPassengers().get(0);
+
+                              // 使用ChatColor转换颜色代码
+                              String titleText = ChatColor.translateAlternateColorCodes('&', text[1]);
+                              String subtitleText = ChatColor.translateAlternateColorCodes('&', text[2]);
+                              String actionbarText = ChatColor.translateAlternateColorCodes('&', text[3]);
+
+                              // 使用带颜色代码的文本发送标题、副标题和动作栏消息
+                              plugin.titleApi.sendTitleWithPlaceholders(p, titleText);
+                              plugin.titleApi.sendSubtitleWithPlaceholders(p, subtitleText);
+                              plugin.titleApi.sendActionbarWithPlaceholders(p, actionbarText);
+                          }
+                      }
                   }
-              }
-              
-              if (text[0].equalsIgnoreCase("[mspenter]")) {
-                  if(cart.getPassengers().size()==1) {
-                	  if(cart.getPassengers().get(0) instanceof Player) {
-                		  Player p = (Player)cart.getPassengers().get(0);
-            			  plugin.titleApi.sendTitleWithPlaceholders(p, "&e即将进站"+text[1]);
-            			  plugin.titleApi.sendSubtitleWithPlaceholders(p,"已进入安全区域");
-            			  plugin.titleApi.sendActionbarWithPlaceholders(p,"感谢乘坐天际轨道交通");
-                	  }
-                  }
-              }
-              
-              if (text[0].equalsIgnoreCase("[mspexit]")) {
-                  if(cart.getPassengers().size()==1) {
-                	  if(cart.getPassengers().get(0) instanceof Player) {
-                		  Player p = (Player)cart.getPassengers().get(0);
-            			  plugin.titleApi.sendTitleWithPlaceholders(p, "&e前方到站:"+text[1]);
-            			  plugin.titleApi.sendSubtitleWithPlaceholders(p,"即将离开安全区域");
-            			  plugin.titleApi.sendActionbarWithPlaceholders(p,"欢迎乘坐轨道交通");
-                	  }
-                  }
-              }
+
               
               if (text[0].equalsIgnoreCase("[s]")) {
                   if(cart.getPassengers().size()==1) {
@@ -154,10 +140,10 @@ public class Minecart_speedplusVehicleListener implements org.bukkit.event.Liste
                     		  //plugin.titleApi.clearTitles(p);
                     		  if(this.line1 < 1.9D) {
                     			  //plugin.titleApi.sendTitleWithPlaceholders(p, " ");
-                    			  plugin.titleApi.sendActionbarWithPlaceholders(p,"&e列车正在减速");
+                    			  plugin.titleApi.sendActionbarWithPlaceholders(p,ChatColor.GOLD + "列车正在减速");
                     		  }else {
                     			  //plugin.titleApi.sendTitleWithPlaceholders(p, " ");
-                    			  plugin.titleApi.sendActionbarWithPlaceholders(p,"&e列车正在加速,请保持前进方向");
+                    			  plugin.titleApi.sendActionbarWithPlaceholders(p,ChatColor.GOLD + "列车正在加速,请保持前进方向");
                     		  }
                     	  }
                       }
